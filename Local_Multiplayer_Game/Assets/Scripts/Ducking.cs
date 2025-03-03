ebc8eba502e568a011a5b82605493bb83dde8d62
MaccccD <Dumisanimofolo@gmail.com>
2025-03-03 17:33:08 +0200

    Ducking Scene | Ducking Sound Update
    -Added the sound effect for when the player ducks in response to an attack.

--- Local_Multiplayer_Game/Assets/Scripts/Ducking.cs
+++ Local_Multiplayer_Game/Assets/Scripts/Ducking.cs
@@ -8,7 +8,7 @@ public class Ducking : MonoBehaviour
     private Vector3 duckScale = new Vector3(1f, 0.5f, 1f);
     private Vector3 playerScale = new Vector3(1f, 1.5f, 1f);
     public float duckHeight = 0.5f; // this value will be adjustable in the inspector based on the tested height that will work with the other melle attack mechanics hence i make it public 
-
+    public AudioSource duckingSound;
 
     public void Duck()
     {
@@ -16,11 +16,13 @@ public class Ducking : MonoBehaviour
         {
             currentSpeed = 0f; 
             DuckingManagement(true); // so allow ducking here
+            duckingSound.Play();
 
         }
         else if (!Input.GetKey(KeyCode.C) && !canDuck) // dont allow ducking
         {
             DuckingManagement(false);
+            // dont play the sound 
         }
     }
     private void DuckingManagement(bool isDucking)
@@ -30,7 +32,8 @@ public class Ducking : MonoBehaviour
             transform.localScale = duckScale;
             transform.position = new Vector3(transform.position.x, transform.position.y - duckHeight, transform.position.z); // the value of this cna be adjudtes based on the actula heght of the duck and how lpw the player can go 
             canDuck = false;
-
+            
+            
         }
 
         else if (!isDucking)
