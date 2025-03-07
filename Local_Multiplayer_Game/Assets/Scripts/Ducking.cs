3ba170826fbf16a89a8d1469fbb1b08176bd9dba
MaccccD <Dumisanimofolo@gmail.com>
2025-03-07 16:49:15 +0200

    Ducking Scene | Ducking Dimensions Update
    -Added the values for the players original height and width as well as the ducking height.

--- Local_Multiplayer_Game/Assets/Scripts/Ducking.cs
+++ Local_Multiplayer_Game/Assets/Scripts/Ducking.cs
@@ -5,9 +5,9 @@ public class Ducking : MonoBehaviour
     public float duckingSpeed;
     public float currentSpeed;
     private bool canDuck = true;
-    private Vector3 duckScale = new Vector3(1f, 0.5f, 1f);
-    private Vector3 playerScale = new Vector3(1f, 1.5f, 1f);
-    public float duckHeight = 0.5f; // this value will be adjustable in the inspector based on the tested height that will work with the other melle attack mechanics hence i make it public 
+    private Vector3 duckScale = new Vector3(1f, 2f, 1f); // player duck dimensions
+    private Vector3 playerScale = new Vector3(1f, 4f, 1f); // player original dimensions with the height of 4 and the width of 1
+    public float duckHeight = 2f; // this value will be adjustable in the inspector based on the tested height that will work with the other melle attack mechanics hence i make it public 
     public AudioSource duckingSound;
 
     public void Duck()
