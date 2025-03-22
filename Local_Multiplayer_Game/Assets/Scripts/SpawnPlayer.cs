c9625ce809f685a4c3bcdd87341209823a14c842
edenneave <edenneave@gmail.com>
2025-03-22 02:39:45 +0200

    FightScreen | Player Differentiation
    changed colour and direction

--- Local_Multiplayer_Game/Assets/Scripts/SpawnPlayer.cs
+++ Local_Multiplayer_Game/Assets/Scripts/SpawnPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class SpawnPlayer : MonoBehaviour
 {
     public Transform[] spawnPoints; //Eden: pts where the players will spawn
+    public Material pinkMaterial; //Eden: temporary material for Player 2 until we have animations
 
     private IEnumerator WaitForPlayerInputManager()
     {
@@ -50,6 +51,41 @@ public class SpawnPlayer : MonoBehaviour
         {
             playerInput.transform.position = spawnPoints[index].position;
         }
-    }
 
+        if (index == 1) //Eden: player 2 index is 1
+        {
+            Renderer playerRenderer = playerInput.gameObject.GetComponent<Renderer>();
+            if (playerRenderer != null && pinkMaterial != null)
+            {
+                playerRenderer.material = new Material(pinkMaterial); //Eden: if there is a p2 and there is a pink material then set material to pink for p2
+                Debug.Log("Applied pink material to Player 2");
+            }
+            else
+            {
+                Debug.LogError("Player 2 does not have a Renderer or the pink material is missing.");
+            }
+
+            //Eden: Because the player has top part as child I need to change material for the first child
+            if (playerInput.transform.childCount > 0)
+            {
+                Renderer firstChildRenderer = playerInput.transform.GetChild(0).GetComponent<Renderer>();
+                if (firstChildRenderer != null && pinkMaterial != null)
+                {
+                    firstChildRenderer.material = new Material(pinkMaterial);
+                    Debug.Log("Applied pink material to Player 2's first child");
+                }
+                else
+                {
+                    Debug.LogError("Player 2's first child does not have a Renderer.");
+                }
+            }
+
+            //Eden: The sword needs to be flipped around therefore I need to move the second child to -1.5 on the x axis
+            if (playerInput.transform.childCount > 1)
+            {
+                Transform secondChild = playerInput.transform.GetChild(1);
+                secondChild.localPosition = new Vector3(-1.5f, secondChild.localPosition.y, secondChild.localPosition.z);
+            }
+        }
+    }
 }
