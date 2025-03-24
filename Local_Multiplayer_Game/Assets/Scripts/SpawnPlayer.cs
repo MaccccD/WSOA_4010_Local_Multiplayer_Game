8ec801a6f743093722850e41a46c04f136470e70
edenneave <edenneave@gmail.com>
2025-03-24 11:12:30 +0200

    Revert "Fight Screen | Audio Set Up Final Update | Audio-Spawn Player Logic."
    This reverts commit 5ecc5a16c23b1801df259db3e094355c928e607a.

--- Local_Multiplayer_Game/Assets/Scripts/SpawnPlayer.cs
+++ Local_Multiplayer_Game/Assets/Scripts/SpawnPlayer.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SpawnPlayer : MonoBehaviour
-
 {
-    public Transform[] spawnPoints;
-    public Material pinkMaterial;
+    public Transform[] spawnPoints; 
+    public Material pinkMaterial;  
 
-    [Header("Audio Sources Feedback")]
+    [Header("Audio  Sources Feedback")]
     [SerializeField] private AudioSource duckSound;
     [SerializeField] private AudioSource jumpingSound;
     [SerializeField] private AudioSource attackSound;
+
+
 
     private void OnEnable()
     {
         if (PlayerInputManager.instance != null)
         {
+
             PlayerInputManager.instance.onPlayerJoined += OnPlayerJoined;
             Debug.Log("SpawnPlayer subscribed to onPlayerJoined.");
         }
@@ -44,24 +46,57 @@ public class SpawnPlayer : MonoBehaviour
 
         ReadyUpSceneManager.Instance?.PlayerJoined(playerInput.gameObject.name);
 
-        // Eden: Spawn the player visually by positioning at the corresponding spawn point
+        //Dumi: The audio sources were getting spawned but they were missing the actual component so i added the compnent of the audio manually ,
+        //that way the audio sources are always assigned.
+        if (index < spawnPoints.Length)
+        // Eden: Spawn the player visually by positioning at the corresponding spawn point.
+   
         if (index < spawnPoints.Length)
         {
             playerInput.transform.position = spawnPoints[index].position;
 
-            // Dumi: Handle audio sources setup. I separated the audio sources from being children of the player to be seperate outside of the player.
+            //Dumi: Since the audio sources were getting spwaned but the sound wasnt playing i took out the audio sources as child of the player and set them up separetely.
             GameObject audioManager = GameObject.FindWithTag("AudioManager");
-            Debug.Log(audioManager + " has been found!!");
+            Debug.Log(audioManager + "hass been found!!");
 
             if (audioManager != null)
             {
-                // Find and assign the Duck Audio Source
+                //D: Logging in  and searching  for all audio sources at once so its  repetetive
+                Debug.Log(audioManager + " has been found!!");
+                //D:  Find the Duck Audio Source comp 1stly
                 AudioSource duckAudioSource = audioManager.transform.Find("Duck Audio Source")?.GetComponent<AudioSource>();
+
+        }
+        else
+        {
+            Debug.LogWarning($"No spawn point defined for player {index}. and no audio srouces have been assigned");
+        }
+
+        if (index == 1)
+        {
+            Renderer playerRenderer = playerInput.gameObject.GetComponent<Renderer>();
+            if (playerRenderer != null && pinkMaterial != null)
+            {
+                playerRenderer.material = new Material(pinkMaterial);
+                Debug.Log("Applied pink material to Player 2.");
+            }
+            else
+            {
+                Debug.LogError("Player 2 does not have a Renderer or the pink material is missing.");
+            }
+
+            if (playerInput.transform.childCount > 0)
+            {
+                Renderer firstChildRenderer = playerInput.transform.GetChild(0).GetComponent<Renderer>();
+                if (firstChildRenderer != null && pinkMaterial != null)
                 if (duckAudioSource != null)
                 {
+                    //D:  Get the Ducking script from the spawned player object since it is an instance of the ori player, it should spawn with the script already.
                     Ducking duckingScript = playerInput.GetComponent<Ducking>();
+
                     if (duckingScript != null)
                     {
+                        //D:  Assigning the Duck Audio Source to the Ducking script's duckingSound variable
                         duckingScript.duckingSound = duckAudioSource;
                         Debug.Log("Duck Audio Source assigned successfully! (1)");
                     }
@@ -72,92 +107,110 @@ public class SpawnPlayer : MonoBehaviour
                 }
                 else
                 {
-                    Debug.LogError("Duck Audio Source not found in AudioManager!");
+                    Debug.LogError("AudioManager GameObject not found in the scene!");
                 }
 
-                // Dumi: Jump Audio setup
-                AudioSource jumpAudioSource = audioManager.transform.Find("Jump Audio Source ")?.GetComponent<AudioSource>();
-                if (jumpAudioSource != null)
+            if (playerInput.transform.childCount > 1)
+            {
+                Transform secondChild = playerInput.transform.GetChild(1);
+                secondChild.localPosition = new Vector3(-1.5f, secondChild.localPosition.y, secondChild.localPosition.z);
+                // Dumi : this is the jump audio stuff:
+                if (audioManager != null)
                 {
-                    Jump jumpingScript = playerInput.GetComponent<Jump>();
-                    if (jumpingScript != null)
-                    {
-                        jumpingScript.jumpingSound = jumpAudioSource;
-                        Debug.Log("Jump Audio Source assigned successfully! (2)");
-                    }
-                    else
-                    {
-                        Debug.LogError("Jumping script not found on the player object.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Jump Audio Source not found in AudioManager!");
-                }
 
-                // Dumi: Attack Audio setup
-                AudioSource attackAudioSource = audioManager.transform.Find("Attack Audio Source")?.GetComponent<AudioSource>();
-                if (attackAudioSource != null)
-                {
-                    HorizontalAttack attackingScript = playerInput.GetComponent<HorizontalAttack>();
-                    if (attackingScript != null)
-                    {
-                        attackingScript.attackSound = attackAudioSource;
-                        Debug.Log("Attack Audio Source assigned successfully! (3)");
-                    }
-                    else
-                    {
-                        Debug.LogError("Attacking script not found on the player object.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Attack Audio Source not found in AudioManager!");
-                }
+                    AudioSource jumpAudioSource = audioManager.transform.Find("Jump Audio Source ")?.GetComponent<AudioSource>();
 
-                // E: For Player 2 (index == 1), apply the pink material and adjust children.
-                if (index == 1)
-                {
-                    Renderer playerRenderer = playerInput.gameObject.GetComponent<Renderer>();
-                    if (playerRenderer != null && pinkMaterial != null)
+                    if (jumpAudioSource != null)
                     {
-                        playerRenderer.material = new Material(pinkMaterial);
-                        Debug.Log("Applied pink material to Player 2.");
-                    }
-                    else
-                    {
-                        Debug.LogError("Player 2 does not have a Renderer or the pink material is missing.");
-                    }
 
-                    // E: Change the material for the first child if available.
-                    if (playerInput.transform.childCount > 0)
-                    {
-                        Renderer firstChildRenderer = playerInput.transform.GetChild(0).GetComponent<Renderer>();
-                        if (firstChildRenderer != null && pinkMaterial != null)
+                        Jump jumpingScript = playerInput.GetComponent<Jump>();
+
+                        if (jumpingScript != null)
                         {
-                            firstChildRenderer.material = new Material(pinkMaterial);
-                            Debug.Log("Applied pink material to Player 2's first child.");
+
+                            jumpingScript.jumpingSound = jumpAudioSource;
+                            Debug.Log("Jump Audio Source assigned successfully! (2)");
                         }
                         else
                         {
-                            Debug.LogError("Player 2's first child does not have a Renderer.");
+                            Debug.LogError("Jumping script not found on the player object.");
                         }
                     }
-
-                    // E: Adjust the second child's local position (for example, to flip a sword).
-                    if (playerInput.transform.childCount > 1)
+                    else
                     {
-                        Transform secondChild = playerInput.transform.GetChild(1);
-                        secondChild.localPosition = new Vector3(-1.5f, secondChild.localPosition.y, secondChild.localPosition.z);
+                        Debug.LogError("AudioManager GameObject not found in the scene!");
+                    }
+                    //D: Attack Audio stuff here :
+                    if (audioManager != null)
+                    {
+
+                        AudioSource attackAudioSource = audioManager.transform.Find("Attack Audio Source")?.GetComponent<AudioSource>();
+
+                        if (attackAudioSource != null)
+                        {
+
+                            HorizontalAttack attackingScript = playerInput.GetComponent<HorizontalAttack>();
+
+                            if (attackingScript != null)
+                            {
+
+                                attackingScript.attackSound = attackAudioSource;
+                                Debug.Log("Attack  Audio Source assigned successfully! (3)");
+                            }
+                            else
+                            {
+                                Debug.LogError("Attacking script not found on the player object.");
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogWarning($"No spawn point defined for player {index}");
+                        }
+
+                        // E: For Player 2 (index == 1), apply the pink material and adjust children.
+                        if (index == 1)
+                        {
+                            Renderer playerRenderer = playerInput.gameObject.GetComponent<Renderer>();
+                            if (playerRenderer != null && pinkMaterial != null)
+                            {
+                                playerRenderer.material = new Material(pinkMaterial);
+                                Debug.Log("Applied pink material to Player 2.");
+                            }
+                            else
+                            {
+                                Debug.LogError("Player 2 does not have a Renderer or the pink material is missing.");
+                            }
+
+                            //E: Change the material for the first child if available.
+                            if (playerInput.transform.childCount > 0)
+                            {
+                                Renderer firstChildRenderer = playerInput.transform.GetChild(0).GetComponent<Renderer>();
+                                if (firstChildRenderer != null && pinkMaterial != null)
+                                {
+                                    firstChildRenderer.material = new Material(pinkMaterial);
+                                    Debug.Log("Applied pink material to Player 2's first child.");
+                                }
+                                else
+                                {
+                                    Debug.LogError("Player 2's first child does not have a Renderer.");
+                                }
+                            }
+
+                            // E: Adjust the second child's local position (for example, to flip a sword).
+                            if (playerInput.transform.childCount > 1)
+                            {
+                                Transform secondChild = playerInput.transform.GetChild(1);
+                                secondChild.localPosition = new Vector3(-1.5f, secondChild.localPosition.y, secondChild.localPosition.z);
+                            }
+                        }
                     }
                 }
             }
-            else
-            {
-                Debug.LogWarning($"No spawn point defined for player {index}. No audio sources have been assigned.");
-            }
         }
-
         DontDestroyOnLoad(playerInput.gameObject);
+
+    }
+}
+
     }
 }
